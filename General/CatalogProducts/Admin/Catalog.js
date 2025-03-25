54b6a745d63df44c197a869ea79dfb75520d285a
Gh0st3r0k <gh0st3r0k@gmail.com>
2025-03-25 18:55:23 +0100

    Add registration other admin

--- General/CatalogProducts/Admin/Catalog.js
+++ General/CatalogProducts/Admin/Catalog.js
@@ -8,13 +8,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const productCardHTML = `
       <div class="col-6 col-sm-4 col-md-4 col-lg-3 mb-3">
         <div class="card h-100 shadow-sm">
-          <img src="../../img/Fon.png" class="card-img-top" alt="Bluetooth Speaker">
+          <img src="../../../img/Fon.png" class="card-img-top" alt="Bluetooth Speaker">
           <div class="card-body p-1 text-center">
             <h5 class="card-title fs-6 fs-md-5 fs-lg-5 fs-xl-4">Bluetooth Speaker</h5>
             <p class="card-text fs-6 fs-md-5 mb-2 mb-sm-3">$29.99</p>
-            <button class="btn btn-outline-dark btn-sm px-3 py-1 fs-6 mb-1">
-              <i class="fas fa-cart-plus me-1"></i>
-              <span class="d-none d-sm-inline">Add to Cart</span>
+            <button class="btn btn-danger btn-sm px-3 py-1 fs-6 mb-1">
+              <i class="fas fa-trash-alt me-1"></i>
+              <span class="d-none d-sm-inline">Delete</span>
             </button>
           </div>
         </div>

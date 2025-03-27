d0c74c37f68033f63be4336c20da9c76f002c0ad
Gh0st3r0k <gh0st3r0k@gmail.com>
2025-03-27 09:39:00 +0100

    Change product card in ProductCatalog

--- General/CatalogProducts/User/Catalog.js
+++ General/CatalogProducts/User/Catalog.js
@@ -8,11 +8,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const productCardHTML = `
       <div class="col-6 col-sm-4 col-md-4 col-lg-3 mb-3">
         <div class="card h-100 shadow-sm">
-          <img src="../../../img/Fon.png" class="card-img-top" alt="Bluetooth Speaker">
-          <div class="card-body p-1 text-center">
-            <h5 class="card-title fs-6 fs-md-5 fs-lg-5 fs-xl-4">Bluetooth Speaker</h5>
-            <p class="card-text fs-6 fs-md-5 mb-2 mb-sm-3">$29.99</p>
-            <button class="btn btn-outline-dark btn-sm px-3 py-1 fs-6 mb-1">
+          <a href="../../ProductCard/ProductCard.html" class="text-decoration-none text-dark">
+            <img src="../../../img/Fon.png" class="card-img-top" alt="Bluetooth Speaker">
+            <div class="card-body p-1 text-center">
+              <h5 class="card-title fs-6 fs-md-5 fs-lg-5 fs-xl-4">Bluetooth Speaker</h5>
+              <p class="card-text fs-6 fs-md-5 mb-2 mb-sm-3">$29.99</p>
+            </div>
+          </a>
+          <div class="text-center pb-2">
+            <button class="btn btn-outline-dark btn-sm px-3 py-1 fs-6"
+                    onclick="event.stopPropagation();">
               <i class="fas fa-cart-plus me-1"></i>
               <span class="d-none d-sm-inline">Add to Cart</span>
             </button>

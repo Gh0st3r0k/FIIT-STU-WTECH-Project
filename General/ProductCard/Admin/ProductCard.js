287b1a63f3a4ad334a78ef1786e69c0e4e0d1feb
KyrylenkoJulia <kirilenko.julia04@gmail.com>
2025-03-27 09:59:34 +0100

    Create ProductCard.js
    same as user

---
+++ General/ProductCard/Admin/ProductCard.js
@@ -0,0 +1,32 @@
+const galleryImages = [
+  "../../../img/test_gal1.jpg",
+  "../../../img/test_gal2.jpg",
+  "../../../img/test_gal3.jpg",
+];
+
+let currentIndex = 0;
+const galleryImg = document.getElementById("galleryImg");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+function showImage(index) {
+  galleryImg.src = galleryImages[index];
+}
+
+prevBtn.addEventListener("click", () => {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = galleryImages.length - 1;
+  }
+  showImage(currentIndex);
+});
+
+nextBtn.addEventListener("click", () => {
+  currentIndex++;
+  if (currentIndex >= galleryImages.length) {
+    currentIndex = 0;
+  }
+  showImage(currentIndex);
+});
+
+showImage(currentIndex);
